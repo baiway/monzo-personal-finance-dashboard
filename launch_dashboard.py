2ea8428749a8d928689f8e0c92511e8255b141c8
Bailey Cook <contact@baileycook.co.uk>
2024-07-24 11:37:34 +0100

    Add function `fetch_all_transactions`
    Removed redudant `src/get_transactions.py`. This was accidentally kept
    in during refactoring. In our lesson on 24th July 2024, we were making
    changes to `src/get_transactions.py` and getting confused why they
    were not being reflected in the code output. This is because our main
    script `launch_dashboard.py` imported the function `fetch_transactions`
    from `monzo_api.py`, i.e. it had no idea about the existence of
    `launch_dashboard.py`. I also changed the function name to
    `fetch_all_transactions` as we may wish to write a different function
    that fetches transactions between a start and end date in the future
    (e.g. to update out cached `transactions.json`).

--- launch_dashboard.py
+++ launch_dashboard.py
@@ -1,5 +1,5 @@
 from src.auth import authenticate
-from src.monzo_api import fetch_transactions
+from src.monzo_api import fetch_all_transactions
 from src.config import load_credentials
 
 if __name__ == "__main__":
@@ -7,10 +7,11 @@ if __name__ == "__main__":
     client_id, client_secret = load_credentials()
     access_token = authenticate(client_id, client_secret)
 
-    # Wait for user to allow this app access their data in the Monzo app
+    # Wait for user to allow this app access their data using the Monzo app
+    # on their phone
     app_auth = ""
     while app_auth != "y":
         app_auth = input("Have you authenticated in the Monzo app? [y/n] ").lower()
 
-    # Fetch all transactions since account creation
-    fetch_transactions(access_token, verbose=True)
+    # Fetch all transactions since account creation and save to transactions.json
+    fetch_all_transactions(access_token, verbose=True)
--- src/monzo_api.py
+++ src/monzo_api.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime, timedelta
 
 
-def fetch_transactions(access_token, verbose=False):
+def fetch_all_transactions(access_token, verbose=False):
     """Retrieves all transactions since the account was created. 
     Saves results in a JSON file: 'transactions.json'.
 
@@ -34,33 +34,46 @@ def fetch_transactions(access_token, verbose=False):
     transactions = []
     print("Attempting to fetch all previous transactions...")
 
+    # Keep requesting transactions in blocks of 100 (the maximum) until we receive
+    # a block with a size less than 100 (at which point we've got them all)
     block_size = 100    
-    while block_size >= 100:
-        # Get block of transactions and append to transactions list
+    while block_size == 100:
         params = {"account_id": account_id,
                   "since": start.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
                   "before": end.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
                   "limit": block_size}
         response = requests.get("https://api.monzo.com/transactions", 
                             headers=header, params=params)
-        tlist = response.json()["transactions"]
-        block_size = len(tlist)
-        transactions.append(tlist)
+        tlist = response.json()["transactions"] # list of JSON-like transaction objects
 
+        # Clean transactions (only keep quantities of interest)
+        qois = ["amount", "created", "categories", "description", "id"]
+        cleaned_transactions = [{key: t[key] for key in qois} for t in tlist]
+
+        # Add cleaned block to full `transactions` list, then determine the size of
+        # the block to check if we need to keep going
+        transactions.extend(cleaned_transactions)
+        block_size = len(cleaned_transactions)
+
+        # Determine date of first and last transaction in the block
         first = tlist[0]["created"]
         last = tlist[-1]["created"]
+
+        # Convert to datetime objects (makes manipulations below easier)
         first = datetime.strptime(first, "%Y-%m-%dT%H:%M:%S.%fZ")
         last = datetime.strptime(last, "%Y-%m-%dT%H:%M:%S.%fZ")
 
-        # Verbose output to show progress (roughly 1 call per second)
+        # Verbose output to show progress (rough 1 API call per second)
+        # Example output line: `24 Jun 2024 to 23 Jul 2024:  83 entries.`
         if verbose:
             print(f"{first.strftime('%d %b %Y')} to {last.strftime('%d %b %Y')}:  " \
                   f"{block_size} entries.")
 
-        # Set start of next block to the end of this block
+        # Set start of next block to the end of this block and end to 1 year later
         start = last + timedelta(seconds=1)
         end = start + timedelta(hours=8760)
 
+    # Save the final `transactions` list
     with open("transactions.json", "w") as file:
         json.dump({"transactions": transactions}, file, indent=2)
 
